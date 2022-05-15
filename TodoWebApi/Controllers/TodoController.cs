b113b652a5e240f1cae8da2c5a56d9e1c5ea609f
Marija Topalova <marijatopalova1@outlook.com>
2022-05-15 22:02:46 +0200

    commit changes

--- TodoWebApi/Controllers/TodoController.cs
+++ TodoWebApi/Controllers/TodoController.cs
@@ -56,7 +56,7 @@ namespace TodoWebApi.Controllers
         }
 
         [HttpDelete("{id}")]
-        public IActionResult Delete(string id)
+        public IActionResult Remove(string id)
         {
             var item = _todoRepository.Find(id);
             if (item == null)
